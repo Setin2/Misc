463e3ffd45d2ea40e411e17391c1a3cfd217980e
Setin2 <68604849+Setin2@users.noreply.github.com>
2022-08-06 10:01:20 +0200

    Adding system calls to UML

--- Operating Systems/(C) Kernel/kernel_msgbox.h
+++ Operating Systems/(C) Kernel/kernel_msgbox.h
@@ -1,8 +1,8 @@
-#ifndef __UML_DM510_MSGBOX_H__
-#define __UML_DM510_MSGBOX_H__
+#ifndef __UML_KERNEL_MSGBOX_H__
+#define __UML_KERNEL_MSGBOX_H__
 
-extern int sys_dm510_msgbox_put( char*, int );
+extern int sys_kernel_msgbox_put( char*, int );
 
-extern int sys_dm510_msgbox_get( char*, int );
+extern int sys_kernel_msgbox_get( char*, int );
 
 #endif
