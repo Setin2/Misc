b6c32224534004f07b4f29256b7643d6cdd4b5cf
Setin2 <68604849+Setin2@users.noreply.github.com>
2022-08-06 10:30:46 +0200

    RLE compression algorithm

--- Operating Systems/Compression&Decompression/RLE.c
+++ Operating Systems/Compression&Decompression/RLE.c
@@ -1,1 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+void main()
+{ 
+    FILE *source = fopen("Test.txt", "r");
+    FILE *destination = fopen("Dest.txt", "w");
+    char currentChar;
+    char seqChar;
+    int count = 0;
+
+    while (1) {
+      int flag = (fread(&currentChar, sizeof(char), 1, source) == 0); 
+
+      if (flag || seqChar != currentChar) {
+
+         if (count >3 ) {
+           char ch = 'Q';
+           int k = count;
+           char str[100];
+           int digits = sprintf(str, "%d", count);
+           fwrite(&ch, sizeof(ch), 1, destination);
+           fwrite(&seqChar, sizeof(ch), 1, destination);
+           fwrite(&str, sizeof(char)*digits, 1, destination);
+         }
+         else {
+           for(int i = 0; i < count; i++) 
+              fwrite(&seqChar, sizeof(char), 1, destination);
+         }
+         seqChar = currentChar;
+         count =1;
+      }
+
+     else count++;
+
+     if(flag)
+       break;
+    }
+
+   fclose(source);
+   fclose(destination);
+}
